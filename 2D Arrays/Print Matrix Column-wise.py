3bedddcf9f488530ef5b73dee2d7d85823aa89c4
Trishansh Sahane <sahanetrishansh@gmail.com>
2026-04-22 07:53:14 +0530

    2D arrays practice

---
+++ 2D Arrays/Print Matrix Column-wise.py
@@ -0,0 +1,13 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+rows = len(matrix)
+cols = len(matrix[0])
+
+for j in range(0,cols): ## column-wise (as column is fixed)
+    for i in range(0,rows): ## row-wise
+        print(matrix[i][j],end=" ")
+    print()
---
+++ 2D Arrays/Print Matrix row wise.py
@@ -0,0 +1,19 @@
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+    ]
+
+# for row in matrix:
+#     for element in row:
+#         print(element,end=" ")
+#     print()
+
+## another way 
+rows = len(matrix)
+cols = len(matrix[0])
+
+for i in range(0,rows): ## row-wise (as row is fixed)
+    for j in range(0,cols): ## column-wise
+        print(matrix[i][j],end=" ")
+    print()
